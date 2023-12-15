032d877727c5cb20de46e6dc4d99fb45971c5a35
Atorami <atoramiseo@mail.ru>
2023-12-15 23:17:21 +0100

    feat: translated to english

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,13 +1,13 @@
 import "./app-info.css";
 
-const AppInfo = ({emplCounter, emplIncr}) => {
-    return (
-        <div className="app-info">
-            <h1>Учет сотрудников в компании N</h1>
-            <h2>Общее число сотрудников: {emplCounter} </h2>
-            <h2>Премию получат: {emplIncr} </h2>
-        </div>
-    )
-}
+const AppInfo = ({ emplCounter, emplIncr }) => {
+  return (
+    <div className="app-info">
+      <h1>Employee Management System</h1>
+      <h2>Number of employees: {emplCounter} </h2>
+      <h2>Will get a promotion: {emplIncr} </h2>
+    </div>
+  );
+};
 
 export default AppInfo;
